6b6fa755b4fd56761eb3c3b9784bb00641ef15b7
BaiceanuVladPavel <pavel.baiceanu@gmail.com>
2024-10-14 14:56:15 +0300

    Delete New folder directory

--- New folder/script.js
+++
@@ -1,9 +0,0 @@
-function checkAnswer(correctAnswer, nextLevel) {
-    const userAnswer = document.getElementById('answer').value.trim();
-    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-        alert('Corect! Te îndrepți spre următorul nivel.');
-        window.location.href = nextLevel;
-    } else {
-        alert('Răspuns greșit! Încearcă din nou.');
-    }
-}
