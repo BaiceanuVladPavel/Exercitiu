20c3521530e21064eef6fbfeba2fb809626b2122
BaiceanuVladPavel <pavel.baiceanu@gmail.com>
2024-10-14 14:54:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function checkAnswer(correctAnswer, nextLevel) {
-    const userAnswer = document.getElementById('answer').value.trim();
-    if (userAnswer.toLowerCase() === correctAnswer.toLowerCase()) {
-        alert('Corect! Te îndrepți spre următorul nivel.');
-        window.location.href = nextLevel;
+function downloadImage(imageUrl) {
+    const a = document.createElement('a');
+    a.href = imageUrl;
+    a.download = 'spike.png'; // Numele fișierului care va fi descărcat
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+function checkAnswers() {
+    const correctAnswers = document.getElementById('answers').value;
+    const message = document.getElementById('message');
+    const minimumCorrect = 5;
+
+    if (correctAnswers >= minimumCorrect) {
+        message.style.color = 'green';
+        message.textContent = "Glückwunsch! Du bist zum nächsten Level gegangen.";
+        window.open('spike.png');
+        window.location.href = "Nivel2.html";
+        
     } else {
-        alert('Răspuns greșit! Încearcă din nou.');
+        message.style.color = 'red';
+        message.textContent = `Du brauchst mindestens ${minimumCorrect} Richtige Antworten, um zum nächsten Level zu gelangen. `;
     }
 }
